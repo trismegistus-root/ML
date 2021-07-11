21b4df9983545cc6cced2a72463cc182bb1ec3cd
Edward <=edward.baldwin@ucdenver.edu>
2021-07-11 13:18:15 -0600

    adding result of iris

--- iris_dataset/main.py
+++ iris_dataset/main.py
@@ -26,3 +26,20 @@ predictions = model.predict(X_validation)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
+
+
+""" [Result]
+0.9666666666666667
+[[11  0  0]
+ [ 0 12  1]
+ [ 0  0  6]]
+                 precision    recall  f1-score   support
+
+    Iris-setosa       1.00      1.00      1.00        11
+Iris-versicolor       1.00      0.92      0.96        13
+ Iris-virginica       0.86      1.00      0.92         6
+
+       accuracy                           0.97        30
+      macro avg       0.95      0.97      0.96        30
+   weighted avg       0.97      0.97      0.97        30
+"""
