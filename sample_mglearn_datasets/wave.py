5af2d77a291608248ec6ef00e6ed0f28e988efca
Edward <=edward.baldwin@ucdenver.edu>
2021-07-11 14:18:34 -0600

    wave...

---
+++ sample_mglearn_datasets/wave.py
@@ -0,0 +1,20 @@
+"""
+To illustrate regression algorithms, 
+this is the synthetic 'wave' dataset.
+The wave dataset has a single input feature 
+and a continuous target variable (or response)
+that we want to model. 
+x-axis: single feature
+y-axis: regression target (output)
+"""
+import mglearn
+from matplotlib import pyplot as plt
+import numpy as np
+
+X,y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X,y,'o')
+plt.ylim(-3,3)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+plt.show()
+
